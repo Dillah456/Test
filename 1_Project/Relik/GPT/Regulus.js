13a71d54213468cdc973a020aae532fef819f099
Dillah456 <145181268+Dillah456@users.noreply.github.com>
2026-07-15 13:43:30 +0700

    Upload Ulang
    Upload ulang proyek Template Relik, dan Redirect Page

---
+++ 1_Project/Relik/GPT/Regulus.js
@@ -0,0 +1,27 @@
+const Regulus = Object.freeze({
+
+    IMPULSE: 0,
+    DOUBT: 1,
+    CONFLICT: 2,
+    DECEPTION: 3,
+
+    Name: [
+        "Impuls",
+        "Keraguan",
+        "Perselisihan",
+        "Kebohongan"
+    ],
+
+    Description: [
+
+        "Dorongan menuju resonansi hawa nafsu.",
+
+        "Gangguan terhadap keyakinan dan kepastian.",
+
+        "Intervensi terhadap relasi dan komunikasi.",
+
+        "Distorsi realitas melalui tipu daya."
+
+    ]
+
+});
